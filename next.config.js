e04cc4bd105574a09dc43a462789d6218925c356
Dispatch <dispatch@cartercodes.dev>
2026-04-08 20:59:47 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-corner-stone-family-dentistry-hope-mills',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
